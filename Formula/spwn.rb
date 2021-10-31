b3e7ea546c05d2a2f895c69d0e6706fab87e1b54
camden314 <camden7306@gmail.com>
2021-10-31 00:53:31 -0500

    initial commit (0.0.7)

---
+++ Formula/spwn.rb
@@ -0,0 +1,11 @@
+class SPWN < Formula
+  desc "Programming language that compiles into Geometry Dash triggers"
+  homepage "https://github.com/Spu7Nix/SPWN-language"
+  url "https://github.com/Spu7Nix/SPWN-language/releases/download/v0.7-beta/spwn-0.7-x86_64-mac.tar.gz"
+  sha256 "8cfaf335a20fde9ddb8790bbc032b4ad10c8a01ab0920000f7a853f4c2ee7271"
+  version "0.0.7"
+
+  def install
+    bin.install "spwn"
+  end
+end
